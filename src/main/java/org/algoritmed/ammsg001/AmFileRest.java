3cb2a2d5b8e84019eaced787fa7390f3e6d45c12
romanm <roman.mishchenko@gmail.com>
2018-10-17 19:39:34 +0300

    file write #5

---
+++ src/main/java/org/algoritmed/ammsg001/AmFileRest.java
@@ -0,0 +1,40 @@
+package org.algoritmed.ammsg001;
+
+import java.security.Principal;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@PropertySource("classpath:a2.properties")
+@Controller
+public class AmFileRest extends XCommon{
+	protected static final Logger logger = LoggerFactory.getLogger(AmFileRest.class);
+	@Autowired protected Environment env;
+
+	
+	@PostMapping("/url_file_write")
+	public @ResponseBody Map<String, Object> url_file_write(
+			@RequestBody Map<String, Object> data
+			,HttpServletRequest request
+			,Principal principal
+		){
+		
+		logger.info("\n\n--27---- "
+				+ "/url_file_write"
+				+ "\n" + data
+				);
+		String amConfigFile = env.getProperty("am.configFile");
+		writeJsonToFile(amConfigFile, data);
+		return data;
+	}
+}
