8c09922cd77404dfa2d07a403c53c73985cf8409
romanm <roman.mishchenko@gmail.com>
2018-12-23 23:50:12 +0200

    x

--- src/main/java/org/algoritmed/ammsg001/amdb/Db1Rest.java
+++ src/main/java/org/algoritmed/ammsg001/amdb/Db1Rest.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class Db1Rest  extends DbCommon{
 	protected static final Logger logger = LoggerFactory.getLogger(DbCommon.class);
